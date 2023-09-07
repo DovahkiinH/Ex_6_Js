599584d16b914553b6d1332190c06de84817e50a
DovahkiinH <loick.caplier@gmail.com>
2023-09-07 09:54:53 +0200

    Ex6Js

---
+++ Assets/Ex6.js
@@ -0,0 +1,24 @@
+/* Script pour concaténer une chaîne de caractères a une variable */
+
+var a = 100;
+
+alert("Ceci est une chaîne de caractères" + " " + a );
+
+/* Script pour une décrémentation */
+
+var b = 100;
+var x = 101;
+
+alert( b = --x );
+
+/* Script pour ajoutez c à a */
+
+var c = 1.00;
+
+alert(a + c);
+
+/* Script pour inverser la valeur de d */
+
+var d = "vrai";
+
+alert(d = "faux");
